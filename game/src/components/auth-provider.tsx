b25a983e49d8ca3a169c99171a10d520a48a3e2b
Aashutosh Rathi <aashutosh@stackrlabs.xyz>
2024-08-06 20:34:45 +0530

    chore: update logo

--- game/src/components/auth-provider.tsx
+++ game/src/components/auth-provider.tsx
@@ -10,7 +10,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
         appearance: {
           theme: "light",
           accentColor: "#676FFF",
-          logo: "https://images.chesscomfiles.com/uploads/v1/article/16910.21d3fdbc.668x375o.934ab09b0f7d@2x.jpeg",
+          logo: "https://dchess.stf.xyz/icon.png",
         },
         embeddedWallets: {
           createOnLogin: "all-users",

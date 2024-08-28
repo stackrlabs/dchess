cdbc58193259a1d5334f64627a94d20cd55c8336
Aashutosh Rathi <aashutosh@stackrlabs.xyz>
2024-08-28 21:52:41 +0530

    feat: add join button on game page

--- game/src/hooks/useAddress.tsx
+++ game/src/hooks/useAddress.tsx
@@ -1,13 +1,10 @@
 import { ZeroAddress } from "@/lib/constants";
 import { formatAddress } from "@/lib/utils";
-import { usePrivy, WalletWithMetadata } from "@privy-io/react-auth";
+import { usePrivy } from "@privy-io/react-auth";
 
 export const useAddress = () => {
   const { user } = usePrivy();
   const walletAddress = user?.wallet?.address;
-  const connectedWallet = (user?.linkedAccounts as WalletWithMetadata[])?.find(
-    (a) => a.connectorType !== "embedded"
-  )?.address!;
 
   const renderString = (address: string) => {
     if (walletAddress === address) {
